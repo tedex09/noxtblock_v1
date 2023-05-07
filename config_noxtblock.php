ed5c5f7835f7aaaddfdad7b303d229d62fc3fad7
tedex09 <119640177+tedex09@users.noreply.github.com>
2023-05-07 18:12:02 -0300

    Add files via upload

---
+++ config_noxtblock.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * NoxtBlock - v1.0
+ * Sistema de Bloqueio contra vazamentos de linhas e outros tipos de ataques
+ * (Desenvolvido por Josemar <gestecgroup@gmail.com>)
+ * -- Proibido a distribuição sem a prévia autorização do desenvolvedor (Evite futuros problemas #).
+ */
+
+// Lista de IPs liberados
+// ** Ao adicionar um novo IP, lembre-se de coloca-lo dentro de aspas e por uma virgula no final
+// *** Exemplo: "1.2.3.4",
+$ips = [ 
+	"38.105.232.197",
+	"38.242.204.243",
+	"95.111.247.213",
+	"209.145.53.142",
+	"159.223.182.29",
+	"165.227.73.188",
+	"159.223.187.13",
+	"5.181.218.108"
+ ];
+ 
+ //Número de Tentativas Erradas para efetuar o bloqueio (Padrão: 10)
+ $tentativas = 10;
+
+/*
+ * Não modifique NADA no código abaixo, caso contrário, o sistema parará de funcionar.
+ */
+
+ $ba9d5787caed81a9 = $ips; $D75c498dcc82bdc3 = $tentativas; define("\x44\x33\x30\x39\x63\x37\x33\64\x39\143\x39\x33\x33\x63\141\x39", $ba9d5787caed81a9); define("\x62\x62\71\x36\61\x37\65\x62\142\x33\x35\x65\60\71\x37\146", $D75c498dcc82bdc3); 
+
+?>
